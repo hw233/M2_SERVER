057561aad8e4c403afea4a77db6e4c6c1d5606eb
JingFeng <gejinnian224@163.com>
2018-04-22 21:20:58 +0800

    更新

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ if [ "$MODE == 'DEBUG'" ]; then
 		rm conf/debug/print.log
 	fi
 	touch conf/debug/print.log
-    nohup ./skynet conf/debug/config.lua > conf/debug/print.log 2>&1 &
+    nohup ./skynet conf/config.lua > conf/debug/print.log 2>&1 &
 else
 	if [ -f "conf/$SERVER/print.log" ]; then
 		rm conf/$SERVER/print.log
 	fi
 	touch conf/$SERVER/print.log
-    nohup ./skynet conf/$SERVER/config.lua > conf/$SERVER/print.log 2>&1 &
+    nohup ./skynet conf/config.lua > conf/$SERVER/print.log 2>&1 &
 fi
 
 
